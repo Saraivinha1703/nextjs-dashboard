765a3ed7515bd8d91f9a62a4c6d8bc3e25cc2547
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2023-12-07 17:36:54 +0000

    ✨ feat: performatic fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Raleway, Lusitana } from 'next/font/google';
+
+export const raleway = Raleway({ subsets: ['latin'] });
+
+//export const lusitana = Lusitana({ weight: '400', subsets: ['latin'] });
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
