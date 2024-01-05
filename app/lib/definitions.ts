8445f137b67493ffcece1ab126137bf26404d5c8
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-01-05 10:32:00 +0000

    ✨ feat: adding types

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -39,6 +39,14 @@ export type LatestInvoice = {
   amount: string;
 };
 
+export type CollectedInvoiceAmount = {
+  amount: number;
+};
+
+export type PendingInvoiceAmount = {
+  amount: number;
+};
+
 // The database returns a number for amount, but we later format it to a string with the formatCurrency function
 export type LatestInvoiceRaw = Omit<LatestInvoice, 'amount'> & {
   amount: number;
